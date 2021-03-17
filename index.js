24c1c1923929b76ee00d9966e63e203e22df3fa2
dev-jrcheitane <66439164+dev-jrcheitane@users.noreply.github.com>
2021-03-18 00:27:05 +0100

    panier fonctionel avec un sessionStorage :)

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // initialisation d'un tableau vide pour stocker les valeur du clique " ajouter au panier"
-let tableCart = [];
-
+let tableCart = []
 
 // Au clique on push l'objet "contentCar" dans le tableau
 $('.add-to-cart').click(function() {
     let $this = $(this);
-    let id = $this.attr('data-id');
-    let name = $this.attr('data-name');
-    let price = $this.attr('data-price');
-    let qte = parseInt($this.attr('data-qte'));
+    let id = $this.attr('data-id')
+    let name = $this.attr('data-name')
+    let price = $this.attr('data-price')
+    let qte = parseInt($this.attr('data-qte'))
 
-    let newArticle = true;
+    let newArticle = true
 
     tableCart.forEach(function(element) {
       // si l'article est déjà présent, on incrémente la quantité
@@ -19,7 +18,7 @@ $('.add-to-cart').click(function() {
           element.qte += qte;
           console.log('qte modifier')
       }
-    });
+    })
 
       if (newArticle) {
         tableCart.push({
@@ -27,33 +26,38 @@ $('.add-to-cart').click(function() {
           name: name,
           price: price,
           qte: qte,
-        });
-    console.log('creation ok')
+        })
+    console.log('creation ok' ,tableCart)
     }
+
+    // on stock le le tableau d'objet dans le sessionStorage
+    sessionStorage.setItem('tableCart',JSON.stringify(tableCart))   
 })
 
-function createParamsModal() {
-  let articles = ''
-  let qte = 0
-  let price = 0
-  
-    tableCart.forEach((element) => {
-      articles = '<p>'+element.name+'</p>'
-      $('#article-modal').append(articles);
-      let itemPrice = element.price.replace(',', '.') * 1000;
-      subTotal = (itemPrice * element.qte) / 1000
-      price = '<p>'+subTotal+'</p>'
-      $('#price-modal').append(price);
-      qte = '<p>'+element.qte+'</p>'
-      $('#qte-modal').append(qte);
-    })
-    
+// remplissage de la modal pour quelle soit prete lors de l'apel 
+
+function displayCart() {
+  $('#article-modal').text('');
+  $('#price-modal').text('');
+  $('#qte-modal').text('');
+
+  JSON.parse(sessionStorage.getItem('tableCart')).forEach((element) => {
+    articles = '<p>'+element.name+'</p>'
+    $('#article-modal').append(articles);
+    let itemPrice = element.price.replace(',', '.') * 1000
+    subTotal = (itemPrice * element.qte) / 1000
+    price = '<p>'+subTotal+'</p>'
+    $('#price-modal').append(price)
+    qte = '<p>'+element.qte+'</p>'
+    $('#qte-modal').append(qte)
+  })
 }
-$('.buy').click(() => {
-  createParamsModal()
-})  
 
+// appel de la modal au click 
 
+$('.buy').click(function() {
+ displayCart()
+ console.log(JSON.parse(sessionStorage.getItem('tableCart')))
+})
 
-
-
+console.log(JSON.parse(sessionStorage.getItem('tableCart')))
