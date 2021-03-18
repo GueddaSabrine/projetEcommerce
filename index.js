d9310af5f31bc6ef4645888f1bf8b888213aecb4
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-03-18 16:51:08 +0100

    panier

--- index.js
+++ index.js
@@ -24,12 +24,10 @@ $(function(){
   })
   });
 
-  // initialisation d'un tableau vide pour stocker les valeur du clique " ajouter au panier"
+// initialisation d'un tableau vide pour stocker les valeur du clique " ajouter au panier"
 let tableCart = []
 
-if (sessionStorage.getItem('tableCart')  == undefined) {
-  console.log('coucou');
-}else {
+if (sessionStorage.getItem('tableCart')  != undefined) {
   badge()
   displayCart()
 }
@@ -43,10 +41,12 @@ $('.add-to-cart').click(function() {
     tableCart = []
   }
 
+
+
     let $this = $(this);
     let id = $this.attr('data-id')
     let name = $this.attr('data-name')
-    let price = $this.attr('data-price')
+    let price = parseInt($this.attr('data-price'))
     let qte = parseInt($this.attr('data-qte'))
 
     let newArticle = true
@@ -76,26 +76,34 @@ $('.add-to-cart').click(function() {
 // remplissage de la modal pour quelle soit prete lors de l'apel 
 
 function displayCart() {
-  $('#article-modal').text('Artcle(s)');
-  $('#price-modal').text('Prix');
-  $('#qte-modal').text('Quantité(s)');
+  
+  let subTt = 0
+  $('#article-modal').text('article');
+  $('#price-modal').text('prix');
+  $('#qte-modal').text('qte');
+  //$('.subtotal').text('')
 
   if(sessionStorage.getItem('tableCart')  != undefined) {
+    subGrandTotal = 0
     JSON.parse(sessionStorage.getItem('tableCart')).forEach((element) => {
       articles = '<p>'+element.name+'</p>'
       $('#article-modal').append(articles);
-      let itemPrice = element.price.replace(',', '.') * 1000
+      let itemPrice = element.price * 1000
       subTotal = (itemPrice * element.qte) / 1000
       price = '<p>'+subTotal+'</p>'
       $('#price-modal').append(price)
       qte = '<p>'+element.qte+'</p>'
       $('#qte-modal').append(qte)
+      subGrandTotal += subTotal
     })
+    $('.subtotal').append(subGrandTotal)
   }
+  
 }
 
 // appel de la modal au click  
 $('.buy').click(function() {
+  $('.subtotal').empty()
  displayCart()
 })
 
@@ -103,14 +111,14 @@ function badge() {
   if(sessionStorage.getItem('tableCart',JSON.stringify(tableCart))  == undefined) {
     $('.badge').text(0);
   } else {
-    let total = JSON.parse(sessionStorage.getItem('tableCart')).reduce((acc, qte ) => acc +=qte.qte , 0)
+    let total = JSON.parse(sessionStorage.getItem('tableCart')).reduce((acc, qte ) => acc += qte.qte , 0)
       $('.badge').text(total);
     };
   }
 
 $('.clearCart').click(function() {
+  $('.subtotal').empty()
   sessionStorage.clear();
   displayCart()
   badge()
 })
-  
